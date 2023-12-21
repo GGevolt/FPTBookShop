e2a613b7ecff9500e92b034abb5b8e48f0cb4fdc
Nguyễn Văn Bảo Duy <117926234+GGevolt@users.noreply.github.com>
2023-12-21 18:34:59 +0700

    Pagination and adjust some html,css

--- BookShop.Models/ViewModel/HomeVM.cs
+++ BookShop.Models/ViewModel/HomeVM.cs
@@ -10,5 +10,10 @@ namespace FPTBookShop.Models.ViewModel
     {
         public List<Book> Books {  get; set; }
         public List<Category>? Categories { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string Search {  get; set; }
+        public string Sort { get; set; }
     }
 }
